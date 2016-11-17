b8220de118a57a163151367a59d5238896bc1101
sub77 <sub77@ymail.com>
2016-11-17 03:24:03 +0000

    msm8226-common: Remove obsolete NITZ system props
    These don't seem to be used anywhere anymore.

--- msm8226.mk
+++ msm8226.mk
@@ -43,17 +43,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rild.nitz_plmn="" \
-    persist.rild.nitz_long_ons_0="" \
-    persist.rild.nitz_long_ons_1="" \
-    persist.rild.nitz_long_ons_2="" \
-    persist.rild.nitz_long_ons_3="" \
-    persist.rild.nitz_short_ons_0="" \
-    persist.rild.nitz_short_ons_1="" \
-    persist.rild.nitz_short_ons_2="" \
-    persist.rild.nitz_short_ons_3=""
-
-PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
 # Audio
