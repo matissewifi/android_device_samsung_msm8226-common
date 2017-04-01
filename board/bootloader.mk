f15efb98aea38c80f56e4b8c48f4bf99570cbd3d
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-01 03:27:17 -0400

    msm8916: Use custom libbootloader-message
    * Use custom libbootloader-message to allow OTA commands to be passed
      properly from system to recovery.

--- board/bootloader.mk
+++ board/bootloader.mk
@@ -1,2 +1,7 @@
 # Bootloader
+<<<<<<< HEAD
 TARGET_BOOTLOADER_BOARD_NAME := MSM8226
+=======
+BOARD_PROVIDES_BOOTLOADER_MESSAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+>>>>>>> 8086fde... msm8916: Use custom libbootloader-message
