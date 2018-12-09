939e69d70084b39119238d2aaf225b93937592f3
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-08 19:30:52 -0500

    Add & merge CPU Architecture/platform
    Add & merge CPU Architecture/platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,12 +16,27 @@
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
 # Platform
-TARGET_BOARD_PLATFORM := msm8226
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+#TARGET_BOARD_PLATFORM := msm8226
+#TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
 
 # Architecture
+#TARGET_ARCH := arm
+#TARGET_ARCH_ABI := armeabi-v7a
+#TARGET_CPU_VARIANT := krait
+
+# Architecture/platform
+BOARD_VENDOR := samsung
+FORCE_32_BIT := true
 TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOARD_PLATFORM_GPU       := qcom-adreno305
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+#TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := krait
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
