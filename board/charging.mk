f97d771b3fd5fdc99cdc71020d99d5aaf89908e9
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-02-21 16:58:46 -0500

    Fixed chagring mode + renamed power.mk > charging.mk

---
+++ board/charging.mk
@@ -0,0 +1,12 @@
+# Charger
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGING_CMDLINE_NAME := "androidboot.bootchg"
+BOARD_CHARGING_CMDLINE_VALUE := "true"
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_NO_CHARGER_LED := true
+
+CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
