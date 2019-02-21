30806bcc8199e748e52321ccc607f3606ebd00b7
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-02-21 08:55:16 -0500

    Most OMX Error Have Been Fixed

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -115,9 +115,9 @@ static char *camera_fixup_getparams(int id, const char *settings)
 #endif
 
     // fix params here
-    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.5");
-    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-4");
-    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "4");
+    // params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.5");
+    // params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-4");
+    // params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "4");
 
     /* If the vendor has HFR values but doesn't also expose that
      * this can be turned off, fixup the params to tell the Camera
