98ad354e4387428086bf516e6839a35bae3c798d
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-06-19 11:58:01 -0400

    Minor fixes fo userdebug build
    Minor fixes fo userdebug build

--- board/dexpreopt.mk
+++ board/dexpreopt.mk
@@ -1,9 +1,7 @@
 # Basic dexpreopt
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-    endif
+  ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_BOOT_IMG_ONLY := true
   endif
 endif
